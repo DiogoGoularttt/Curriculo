917e7fe8d7061adaaf7fcc3566408ee37198ed42
Diogo Goulart <136640390+DiogoGoularttt@users.noreply.github.com>
2025-03-07 19:25:12 -0300

    :rocket: feat: Implement sidebar in mobile

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,33 @@
-const botao = document.querySelector(".btn-menu");
+const botaoAbrir = document.querySelector(".fa-bars");
+const botaoFechar = document.querySelector(".fa-xmark");
+const overlay = document.querySelector(".overlay");
+const itemsMenu = document.querySelector(".menu-items");
+let isMenuOpen = false;
 
 const openMenu = () => {
-  alert("O menu está aberto!");
+  if (isMenuOpen == false) {
+    itemsMenu.style.display = "flex";
+    overlay.style.display = "block";
+    botaoAbrir.style.display = "none";
+    botaoFechar.style.display = "block";
+    isMenuOpen = true;
+  } else if (isMenuOpen == true) {
+    itemsMenu.style.display = "none";
+    overlay.style.display = "none";
+    botaoFechar.style.display = "none";
+    botaoAbrir.style.display = "block";
+    isMenuOpen = false;
+  }
 };
 
-botao.addEventListener("click", () => {
+botaoAbrir.addEventListener("click", () => {
+  openMenu();
+});
+
+botaoFechar.addEventListener("click", () => {
+  openMenu();
+});
+
+overlay.addEventListener("click", () => {
   openMenu();
 });
